5c2ff12a716b139c852998460d4db0e3d2ad17eb
Akeju Paul <akejupaul10@gmail.com>
2022-01-16 13:16:55 +0100

    hero

---
+++ src/components/Home/hero.home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div style="height: 100vh" class="black">
+      <v-row style="height: 100%; position: absolute; width: 100%" no-gutters>
+        <v-col cols="3" xl="5" lg="5"> </v-col>
+        <v-col
+          :style="`background:url(${require('../../assets/images/bg.png')});background-size:cover`"
+          cols="9"
+          xl="4"
+          lg="4"
+        >
+        </v-col>
+      </v-row>
+      <v-container>
+        <v-row
+          style="height: 100vh; position: relative; z-index: 1"
+          no-gutters
+          align="center"
+          justify="center"
+        >
+          <v-col cols="12" xl="6" lg="6">
+            <p
+              class="white--text font-size-2 font-weight-bold"
+              style="line-height: 1"
+            >
+              Get the best part of your day <br /><span style="color: #4ae290">
+                – you fit here.</span
+              >
+            </p>
+            <p class="white--text font-size-8" style="line-height:1.4">
+              We provide serious fitness but within <br> a fun and friendly, safe
+              space.
+            </p>
+            <v-btn color="white" class="font-weight-bold" style="border-radius:0" large>JOIN NOW</v-btn>
+          </v-col>
+          <v-col cols="12" xl="6" lg="6" class="d-none d-sm-flex" />
+        </v-row>
+      </v-container>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/Home/index.home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <Hero />
+  </div>
+</template>
+
+<script>
+import Hero from './hero.home.vue'
+export default {
+    components:{
+        Hero
+    }
+}
+</script>
+
+<style>
+
+</style>
